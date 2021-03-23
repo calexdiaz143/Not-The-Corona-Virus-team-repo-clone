30aeed48815bf3fa9369fbc5bbc01f83ad6a87dc
Timmy <timmyngo021@gmail.com>
2021-03-23 14:11:02 -0700

    added text to dataframe function

--- Challenge2/splitText.py
+++ Challenge2/splitText.py
@@ -1,5 +1,6 @@
 from tkinter.filedialog import askopenfilename
-
+import pandas as pd
+import numpy as np
 """ Takes in input text, splits by delimiter, returns as dictionary with headings as keys 
     output example: {'Things to know about the COVID-19 Pandemic':'Three Important Ways to Slow the Spread', 'Wear a mask to protect yourself and others and stop the spread of COVID-19.', ''}
 """
@@ -9,16 +10,33 @@ def splitText(text, delimiter):
     for line in textArray:
         # finds the first line in the section and uses that as the heading
         firstNewlineIndex = line.find("\n")
+        header = line[0:firstNewlineIndex]
         # adds remaining lines to dictionary, replace("\xa0", " ") added in to get rid of weird symbol
-        textDictionary[line[0:firstNewlineIndex]] = (line[firstNewlineIndex + 1:]).replace("\xa0", " ").split("\n")
+        textDictionary[header] = (line[firstNewlineIndex + 1:]).replace("\xa0", " ").split("\n")
+        
     return textDictionary
 
+""" Takes in input text, splits by delimiter, returns as pandas dataframe with headings as keys e.g. [index, heade, text]
+"""
+def textToDataFrame(text, delimiter):
+    textArray = text.split(delimiter)
+    df = pd.DataFrame(columns=["header", "text"])
+    for line in textArray:
+        # finds the first line in the section and uses that as the heading
+        firstNewlineIndex = line.find("\n")
+        header = line[0:firstNewlineIndex]
+        # puts the remaining text into dataframe
+        df2 = pd.DataFrame({'header': header, 'text':(line[firstNewlineIndex + 1:]).replace("\xa0", " ").split("\n")})
+        # combines new dataframe with the return dataframe
+        df = df.append(df2)
+    return df
 
-filename = askopenfilename()
-# with open("C:/Users/Timmy/Documents/work/hackathon/Not-The-Corona-Virus-team-repo/Challenge2/data/CDCGuidelines.txt", encoding="utf8") as myFile:
-with open(filename, encoding="utf8") as myFile:
+
+
+with open("C:/Users/Timmy/Documents/work/hackathon/Not-The-Corona-Virus-team-repo/Challenge2/data/CDCGuidelines.txt", encoding="utf8") as myFile:
+# filename = askopenfilename()
+# with open(filename, encoding="utf8") as myFile:
     data = myFile.read()
-    docArray = splitText(data, "***")
-    
-    print(docArray["Symptoms of Coronavirus"])
 
+    dfRes = textToDataFrame(data, "***")
+    print(dfRes)
